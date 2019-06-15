f2ed031aff20bc56af6715b30d58c5bf21527fd4
eugenia ong <eugenia.osy@gmail.com>
2019-06-15 15:23:36 -0700

    Update db connection file' '

--- db/index.js
+++ db/index.js
@@ -3,7 +3,6 @@ const pg = require('pg');
 const { Pool } = pg;
 const pool = new Pool({
   user: 'postgres',
-  password: 'postgres',
   host: '54.147.79.115',
   database: 'bookings',
   port: 5432,
